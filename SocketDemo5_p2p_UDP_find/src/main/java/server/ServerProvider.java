b226d3648ec2c241e87d5d154285af4d99703634
Jesen0823 <xie_stacol@163.com>
2024-11-21 10:38:44 +0800

    [09] add 'SocketDemo5_p2p_UDP_find'

---
+++ SocketDemo5_p2p_UDP_find/src/main/java/server/ServerProvider.java
@@ -0,0 +1,120 @@
+package server;
+
+import clink.net.qiujuer.clink.utils.ByteUtils;
+import constants.UDPConstants;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class ServerProvider {
+    private static Provider PROVIDER_INSTANCE;
+
+    static void start(int port) {
+        stop();
+        String sn = UUID.randomUUID().toString();
+        Provider provider = new Provider(sn, port);
+        provider.start();
+        PROVIDER_INSTANCE = provider;
+    }
+
+    static void stop() {
+        if (PROVIDER_INSTANCE != null) {
+            PROVIDER_INSTANCE.exit();
+            PROVIDER_INSTANCE = null;
+        }
+    }
+
+    private static class Provider extends Thread {
+        private final byte[] sn;
+        private final int port;
+        private boolean done = false;
+        private DatagramSocket ds = null;
+
+        final byte[] buffer = new byte[128];
+
+        public Provider(String sn, int port) {
+            super();
+            this.port = port;
+            this.sn = sn.getBytes();
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+            System.out.println("Provider start");
+
+            try {
+                // 监听服务器端口
+                ds = new DatagramSocket(UDPConstants.PORT_SERVER);
+                DatagramPacket receivePack = new DatagramPacket(buffer, buffer.length);
+
+                while (!done) {
+                    // 接收
+                    ds.receive(receivePack);
+
+                    // 打印发送者的ip，接收到的信息
+                    String clientIp = receivePack.getAddress().getHostAddress();
+                    int clientPort = receivePack.getPort();
+                    int clientDataLen = receivePack.getLength();
+                    byte[] clientData = receivePack.getData();
+
+                    boolean isValid = clientDataLen >= (UDPConstants.HEADER.length + 2 + 4) && ByteUtils.startsWith(clientData, UDPConstants.HEADER);
+                    System.out.println("Provider receive form ip:" + clientIp
+                            + "\tport:" + clientPort + "\tdataValid:" + isValid);
+                    if (!isValid) {
+                        // 无效继续
+                        continue;
+                    }
+                    // 解析命令与回送端口
+                    int index = UDPConstants.HEADER.length;
+                    short cmd = (short) ((clientData[index++] << 8) | (clientData[index++] & 0xff));
+                    int responsePort = (((clientData[index++]) << 24) |
+                            ((clientData[index++] & 0xff) << 16) |
+                            ((clientData[index++] & 0xff) << 8) |
+                            ((clientData[index] & 0xff)));
+
+                    // 判断合法性
+                    if (cmd == 1 && responsePort > 0) {
+                        // 构建一份回送数据
+                        ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
+                        byteBuffer.put(UDPConstants.HEADER);
+                        byteBuffer.putShort((short) 2);
+                        byteBuffer.putInt(port);
+                        byteBuffer.put(sn);
+                        int len = byteBuffer.position();
+                        // 直接根据发送者构建一份回送信息
+                        DatagramPacket responsePacket = new DatagramPacket(buffer,
+                                len,
+                                receivePack.getAddress(),
+                                responsePort);
+                        ds.send(responsePacket);
+                        System.out.println("Provider response to:" + clientIp + "\tport:" + responsePort + "\tdataLen:" + len);
+                    } else {
+                        System.out.println("Provider receive cmd nonsupport; cmd:" + cmd + "\tport:" + port);
+                    }
+                }
+            } catch (Exception e) {
+
+            } finally {
+                close();
+            }
+            // 完成
+            System.out.println("Provider Finished.");
+        }
+
+        public void exit() {
+            done = true;
+            close();
+        }
+
+        private void close() {
+            if (ds != null) {
+                ds.close();
+                ds = null;
+            }
+        }
+    }
+}
