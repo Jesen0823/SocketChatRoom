876f35526437de612d0a148d41f5e2a7779d01f5
Jesen0823 <xie_stacol@163.com>
2024-11-23 10:22:31 +0800

    [24] add "SocketChatRoom_T10_concurrent" copy from "SocketChatRoom_T9_file"

---
+++ SocketChatRoom_T10_concurrent/lib-clink/src/main/java/net/qiujuer/library/clink/core/SendDispatcher.java
@@ -0,0 +1,15 @@
+package net.qiujuer.library.clink.core;
+
+import java.io.Closeable;
+
+/**
+ * 发送数据调度者
+ * 缓存需要发送的数据，通过队列对待发数据进行发送
+ * 发送数据时做一个包装
+ */
+public interface SendDispatcher extends Closeable{
+
+    void send(SendPacket packet);
+
+    void cancel(SendPacket packet);
+}
---
+++ SocketChatRoom_T10_concurrent/lib-clink/src/main/java/net/qiujuer/library/clink/impl/async/AsyncReceiveDispatcher.java
@@ -0,0 +1,88 @@
+package net.qiujuer.library.clink.impl.async;
+
+import net.qiujuer.library.clink.core.IoArgs;
+import net.qiujuer.library.clink.core.ReceiveDispatcher;
+import net.qiujuer.library.clink.core.ReceivePacket;
+import net.qiujuer.library.clink.core.Receiver;
+import net.qiujuer.library.clink.utils.CloseUtils;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class AsyncReceiveDispatcher implements ReceiveDispatcher, IoArgs.IoArgsEventListener, AsyncPacketWriter.PacketProvider {
+
+    private final AtomicBoolean isClosed = new AtomicBoolean(false);
+    private final Receiver receiver;
+    private final ReceivePacketCallback receiveCallback;
+    private final AsyncPacketWriter packetWriter = new AsyncPacketWriter(this);
+
+
+    public AsyncReceiveDispatcher(Receiver receiver, ReceivePacketCallback receiveCallback) {
+        this.receiver = receiver;
+        this.receiver.setReceiveListener(this);
+        this.receiveCallback = receiveCallback;
+    }
+
+    @Override
+    public void start() {
+        registerReceive();
+    }
+
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (isClosed.compareAndSet(false, true)) {
+           packetWriter.close();
+        }
+    }
+
+    private void registerReceive() {
+        try {
+            receiver.postReceiveAsync();
+        } catch (Exception e) {
+            e.printStackTrace();
+            closeAndNotify();
+        }
+    }
+
+    private void closeAndNotify() {
+        CloseUtils.close(this);
+    }
+
+    @Override
+    public IoArgs provideIoArgs() {
+        return packetWriter.takeIoArgs();
+    }
+
+    @Override
+    public boolean onConsumeFailed(IoArgs args, Throwable e) {
+        return false;
+    }
+
+    @Override
+    public boolean onConsumeCompleted(IoArgs args) {
+        do {
+            packetWriter.consumeIoArgs(args);
+        }while (args.remained());
+
+        // 接收下一次数据
+        registerReceive();
+        return true;
+    }
+
+    @Override
+    public ReceivePacket takePacket(byte type, long length, byte[] headerInfo) {
+        return receiveCallback.onArrivedNewPacket(type, length);
+    }
+
+    @Override
+    public void completedPacket(ReceivePacket packet, boolean isSucceed) {
+        CloseUtils.close(packet);
+        receiveCallback.onReceivePacketCompleted(packet);
+    }
+}
