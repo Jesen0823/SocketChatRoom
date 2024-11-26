034434f10257d5675a26e0137ee416548afd7e3f
Jesen0823 <xie_stacol@163.com>
2024-11-26 09:24:05 +0800

    [29] bug fix

--- SocketChatRoom_T10_concurrent/sample-server/src/main/java/net/qiujuer/lesson/sample/server/TCPServer.java
+++ SocketChatRoom_T10_concurrent/sample-server/src/main/java/net/qiujuer/lesson/sample/server/TCPServer.java
@@ -120,7 +120,7 @@ public class TCPServer implements ServerAcceptor.AcceptListener, Group.GroupMess
             // 添加关闭操作的责任链节点
             clientHandler.getCloseChain().appendLast(new RemoveQueueOnConnectorClosedChain());
 
-            synchronized (TCPServer.this) {
+            synchronized (clientHandlerList) {
                 clientHandlerList.add(clientHandler);
                 System.out.println("当前客户端数量：" + clientHandlerList.size());
             }
